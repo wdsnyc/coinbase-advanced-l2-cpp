9080c4d7cdf99369f068954ad18464b2a7cd2645
Bill Steinberg <wds@panix.com>
2026-07-29 11:05:35 -0400

    Add build scripts cmake option to generate compile_commands.json
    json file used by clangd and clang-tidy to provide accurate
    navigation, diagnostics, and refactoring support

--- scripts/run_cmake_gen.sh
+++ scripts/run_cmake_gen.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -x
 
-VERBOSE=1 cmake -DCMAKE_BUILD_TYPE=Debug -B build
+# CMAKE_EXPORT_COMPILE_COMMANMDS generates compile_commands.json used
+# by emacs clangd/lsp mode
+
+VERBOSE=1 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -B build
